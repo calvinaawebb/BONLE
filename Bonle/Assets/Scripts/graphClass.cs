7d0ad8823f6dfb81d18fe8277c95fa55af276a49
CentreFusion <calvinaawebb@gmail.com>
2023-02-11 00:35:31 -0700

    Day of Work 2/10/2023
    I made my code less cancer today by making it so that I only have to have one script that holds the logic and it gets passed in the graphs as it goes. Worked a bunch on the new UI, still trying to figure out the stupid animations.

---
+++ Bonle/Assets/Scripts/graphClass.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graphClass : MonoBehaviour
+{
+    public ArrayList node_list = new ArrayList();
+    public Dictionary<string, double> valuePairs = new Dictionary<string, double>();
+}
--- Bonle/Assets/Scripts/loadScene.cs
+++ Bonle/Assets/Scripts/loadScene.cs
@@ -6,10 +6,27 @@ using UnityEngine.SceneManagement;
 public class loadScene : MonoBehaviour
 {
     public string scene;
+    public Animator transistion;
+    public static float transistionTime = 1.1f;
 
     // Start is called before the first frame update
     public void sceneLoad()
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void startLevel() 
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel() 
+    {
+        transistion.SetTrigger("out");
+
+        yield return new WaitForSeconds(transistionTime);
+
+        sceneLoad();
+
     }
 }
