b00670cb07f7b6e6f25f3915c45783ab79df7df5
FluxChikin <calvinaawebb@gmail.com>
2023-10-01 23:15:40 -0600

    Weekly Update

--- Bonle/Assets/Scripts/TabGroup.cs
+++ Bonle/Assets/Scripts/TabGroup.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TabGroup : MonoBehaviour
 {
     public List<TabButton> tabButtons;
+    public List<TabButton> backgrounds;
+    public Sprite tabIdle;
+    public Sprite tabHover;
+    public Sprite tabActive;
+    public Sprite backIdle;
+    public Sprite backHover;
+    public Sprite backActive;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+    //public List<GameObject> backgroundsToSwap;
 
     // Start is called before the first frame update
-    void Start()
+    public void Subscribe(TabButton button)
     {
         if (tabButtons == null) 
         {
@@ -15,20 +26,61 @@ public class TabGroup : MonoBehaviour
         }
 
         tabButtons.Add(button);
+        backgrounds.Add(button.back);
     }
 
     public void OnTabEnter(TabButton button) 
     {
-
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab) 
+        {
+            button.background.sprite = tabHover;
+            button.back.background.sprite = backHover;
+        }
     }
 
     public void OnTabExit(TabButton button)
     {
-
+        ResetTabs();
     }
 
     public void OnTabSelected(TabButton button) 
     {
+        if (selectedTab != null) 
+        {
+            selectedTab.Deselect();
+        }
+        selectedTab = button;
+        selectedTab.Select();
+        //selectedTab.back.Select();
+        ResetTabs();
+        button.background.sprite = tabActive;
+        button.back.background.sprite = backActive;
+        int index = button.transform.GetSiblingIndex();
+        for (int i = 0; i < objectsToSwap.Count; i++) 
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else 
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
 
+    public void ResetTabs() 
+    {
+        foreach(TabButton button in tabButtons) 
+        {
+            if (selectedTab != null && button == selectedTab) { continue; }
+            button.background.sprite = tabIdle;
+            foreach (TabButton backg in backgrounds)
+            {
+                //if (selectedTab != null && button == selectedTab) { continue; }
+                backg.background.sprite = backIdle;
+            }
+        }
     }
 }
